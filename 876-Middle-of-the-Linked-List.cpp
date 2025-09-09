9c7b1e032db2f3263b38773e7a97025f562b8760
hakan <hakanntemiz@gmail.com>
2025-09-09 13:45:40 +0300

    Update 876-Middle-of-the-Linked-List.cpp

--- 876-Middle-of-the-Linked-List.cpp
+++ 876-Middle-of-the-Linked-List.cpp
@@ -57,3 +57,59 @@ int main(){
     printList(middle);
     deleteList(head);
 }
+/*
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct ListNode{
+    int val;
+    ListNode* next;
+    ListNode(int x){
+        val = x;
+        next = nullptr;
+    }
+};
+
+void printList(ListNode* head) {
+    ListNode* current = head;
+    while (current != nullptr) {
+        cout << current->val << " -> ";
+        current = current->next;
+    }
+    cout << "nullptr" << endl;
+}
+
+ListNode* createList(const vector<int> &values){
+    ListNode* head = new ListNode(values[0]);
+    ListNode* current = head;
+    for (int i=1; i<values.size(); i++){
+        current -> next = new ListNode(values[i]);
+        current = current -> next;
+    }
+    return head;
+}
+
+ListNode* middle(ListNode* head){
+    int eleman_sayisi = 0;
+    ListNode* current = head;
+    while (current != nullptr){
+        eleman_sayisi ++;
+        current = current -> next;
+    }
+    int middle_index = eleman_sayisi / 2;
+    current = head;
+    for (int i = 0; i < middle_index; i++) {
+        current = current->next;
+    }
+    return current;
+}
+
+int main(){
+    vector <int> sayilar = {1,2,3,4,5,6};
+    ListNode* head = createList(sayilar);
+    ListNode* mid = middle(head);
+    cout << mid -> val;
+}
+*/
